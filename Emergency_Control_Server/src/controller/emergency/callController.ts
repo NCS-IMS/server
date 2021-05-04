94fd9a49cb345c59a936a5d251360a33a1569da9
Positive-Conative <kyj970513@naver.com>
2021-05-04 06:26:31 +0000

    Create : request middleware

--- Emergency_Control_Server/src/controller/emergency/callController.ts
+++ Emergency_Control_Server/src/controller/emergency/callController.ts
@@ -13,7 +13,8 @@ async function callMain(req: Request, res: Response) {
             "latitude": req.body.latitude,
             "longitude": req.body.longitude,
         }
-        await sendPushMessageIndividual(`${bodyData.state}환자 발생!!`, `${await find_userLocation(bodyData)}에 응급상황이 발생하였습니다.`, [
+        let userLocation: any = await find_userLocation(bodyData);
+        await sendPushMessageIndividual(`${bodyData.state}환자 발생!!`, `${userLocation.documents[0].address.address_name}에 응급상황이 발생하였습니다.`, [
             "edJhijkqQ-mEgTt7bONnrh:APA91bGPHLda78vBSVzCqqZTk0ij4iu8x0m4nc27pzRsbF7xzV6LCymFZYOlvezoWbQK_CwmIbwgf_cVI-BHPy5gSq9hesyWe04TbpMUDl7T92k4MA9eaH5fOCNgSuGDMARlfpfXzEnm",
             "f6_TfMnCQUG6ut7sbTLziX:APA91bGgcN1sjZ5WBYgL_67ITKirjoFVYCGwWAI9ZX9eTM0OUF8oZdiKbu1fn9kSEo7NapvwzN407Zz5nsZg-zXEnFjzem4ZZkfnMgOV1LYBy93HNH7NUNXNPCCc2ao3P4u--0yp8RGB"
         ])  //푸시메시지 전송
@@ -32,8 +33,6 @@ async function callMain(req: Request, res: Response) {
     }catch(errMsg){
         res.status(500).json({ "message": errMsg })
     }
-    // sendPushMessageGroup("asdf","fdsa","test")
-    
 }
 
 export {
--- Emergency_Control_Server/src/service/apis/findLocation.ts
+++ Emergency_Control_Server/src/service/apis/findLocation.ts
@@ -1,29 +1,16 @@
 import { callLogDto } from "../../interface/callLogDto";
 import { logger } from "../../config/logger";
-import request from 'request-promise-native';
+import requestModule from "../../middleware/request"
 
 //병원 위치 찾기
 async function find_hospital(bodyData: callLogDto) {
   try {
-    let group_cord: string = "HP8";
-    let radius: number = 20000;
 
-    const options = { //option 지정
-      uri:`https://dapi.kakao.com/v2/local/search/category.json?category_group_code=${group_cord}&radius=${radius}&x=${bodyData.latitude}&y=${bodyData.longitude}`,
-      method: 'GET',
-      // body: {
-      //   kakaoId:'f',
-      // },
-      json:true,
-      headers: { Authorization: `KakaoAK ${process.env.KAKAO_API_KEY}` },
-    }
-    let a :any ;
-    return request(options)
-    .then(
-      (result:any)=>{
-        return result;
-      }
-    )
+    let group_cord: string = "HP8"; //병원
+    let radius: number = 20000;     //범위
+    let parseUrl = `https://dapi.kakao.com/v2/local/search/category.json?category_group_code=${group_cord}&radius=${radius}&x=${bodyData.latitude}&y=${bodyData.longitude}`
+    return requestModule(parseUrl, 'GET', 'kakao')
+    
   }catch (errMsg){
     logger.error({
       label: "[findHospital.ts - find_hospital]",
@@ -36,22 +23,8 @@ async function find_hospital(bodyData: callLogDto) {
 //사용자 위치 확인
 async function find_userLocation(bodyData: callLogDto) {
   try {
-
-    const options = { //option 지정
-      uri:`https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${bodyData.longitude}&y=${bodyData.latitude}`,
-      method: 'GET',
-      // body: {
-      //   kakaoId:'f',
-      // },
-      json:true,
-      headers: { Authorization: `KakaoAK ${process.env.KAKAO_API_KEY}` },
-    }
-    return request(options)
-    .then(
-      (result:any)=>{
-        return result.documents[0].address.address_name;  //뽑기
-      }
-    )
+    let parseUrl = `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${bodyData.longitude}&y=${bodyData.latitude}`
+    return requestModule(parseUrl, 'GET', 'kakao')
 
   }catch (errMsg){
     logger.error({
